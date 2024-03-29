6a09102094b62471bd4d206b03f5abf486b2629c
Antony Mangala <aman0083@student.monash.edu>
2024-03-29 16:30:42 +1100

    Added login functionality

---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'email' => 'Lorem ipsum dolor sit amet',
+                'password' => 'Lorem ipsum dolor sit amet',
+                'nonce' => 'Lorem ipsum dolor sit amet',
+                'nonce_expiry' => '2024-03-29 04:53:05',
+                'created' => '2024-03-29 04:53:05',
+                'modified' => '2024-03-29 04:53:05',
+            ],
+        ];
+        parent::init();
+    }
+}
