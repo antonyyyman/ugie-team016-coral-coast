89c939e54f540e410a6ae62934db398756645f89
Andres <abal0060@student.monash.edu>
2024-04-15 16:40:47 +1000

    Added dashboard functionality with routing

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -1,11 +1,6 @@
 <?php
 namespace App\Controller;
 
-use Cake\Core\Configure;
-use Cake\Network\Exception\NotFoundException;
-use Cake\View\Exception\MissingTemplateException;
-use Cake\Datasource\ConnectionManager;
-
 class DashboardController extends AppController
 {
 
