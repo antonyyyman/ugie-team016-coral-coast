6d6d70814ec7db8be3a87ab871e61b1a2f9700a8
Andres <abal0060@student.monash.edu>
2024-04-15 18:15:06 +1000

    Updated it to main and also started on bootstrap functionality for the dashboard

--- templates/Dashboard/index.php
+++ templates/Dashboard/index.php
@@ -2,6 +2,14 @@
 <html>
 <head>
     <title>Dashboard</title>
+    <link href="css/styles.scss" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+          rel="stylesheet">
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.5.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body>
 <h1>Dashboard</h1>
