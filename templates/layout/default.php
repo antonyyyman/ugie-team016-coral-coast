b4eccf9794e3d7e56f31edc94be9f916b9f0b1de
Tristan Pantano <tpan0033@student.monash.edu>
2024-04-16 23:05:48 +1000

    Fixed up links to home page, added login/logout button to nav bar although nav bar not showing on home page yet

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -54,12 +54,14 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
 <header id="menu-jk" class="container-fluid">
         <div class="row">
             <div class="col-md-3 logo">
-                <img src="webroot/img/logo_coralcoast.png" alt="">
+            <a href="/team016-app_fit3047/pages/home">
+                <img src="webroot/img/logo_coralcoast.png" alt="Coral Coast Logo">
+            </a>
                  <a data-toggle="collapse" data-target="#menu" href="#menu"><i class="fas d-block d-lg-none  small-menu fa-bars"></i></a>
             </div>
             <div id="menu" class="col-lg-6 col-md-9 d-none d-md-block navs">
                 <ul>
-                    <li><a href="index.html">Home</a></li>
+                    <li><a href="/team016-app_fit3047/pages/home">Home</a></li>
                     <li><a href="about_us.html">About Us</a></li>
                     <li><a href="packages.html">Packages</a></li>
                     <li><a href="destination.html">Destinations</a></li>
@@ -68,20 +70,19 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
                 </ul>
             </div>
             <div class="col-md-3 d-none d-lg-block socila-link">
-                <ul>
-                    <li>
-                        <i class="fab fa-facebook-square"></i>
-                    </li>
-                    <li>
-                        <i class="fab fa-twitter-square"></i>
-                    </li>
-                    <li>
-                        <i class="fab fa-instagram"></i>
-                    </li>
-                    <li>
-                        <i class="fab fa-linkedin"></i>
-                    </li>
-                </ul>
+            <?php
+                if ($this->Identity->isLoggedIn()) {
+                echo $this->Html->link(
+                    'Logout',
+                    ['controller' => 'Auth', 'action' => 'logout'],
+                    ['class' => 'button button-outline']);
+                } else{
+                    echo $this->Html->link(
+                    'Log in',
+                    ['controller' => 'Auth', 'action' => 'login'],
+                    ['class' => 'button button-outline']);
+                }
+            ?>
             </div>
         </div>
     </header>
