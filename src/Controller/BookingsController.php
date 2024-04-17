9446b324ae9138acbf3fc7aeb4fe8cea1611135b
Antony Mangala <aman0083@student.monash.edu>
2024-04-17 20:54:30 +1000

    resolved conflicts from rebase with main

--- src/Controller/BookingsController.php
+++ src/Controller/BookingsController.php
@@ -20,7 +20,6 @@ class BookingsController extends AppController
     public function index()
     {
         $query = $this->Bookings->find()
-<<<<<<< HEAD
             ->contain(['Users', 'Hotels', 'CarRentals', 'Insurances', 'Translations', 'Payments', 'TravelDeals']);
 
         if (!empty($this->request->getQuery('id'))) {
@@ -50,9 +49,6 @@ class BookingsController extends AppController
         }
 
 
-=======
-            ->contain(['Users', 'Payments', 'Insurances', 'Hotels', 'CarRentals', 'Translations']);
->>>>>>> c7a35e9 (initial commit of baked files)
         $bookings = $this->paginate($query);
 
         $this->set(compact('bookings'));
@@ -67,7 +63,7 @@ class BookingsController extends AppController
      */
     public function view($id = null)
     {
-        $booking = $this->Bookings->get($id, contain: ['Users', 'Payments', 'Insurances', 'Hotels', 'CarRentals', 'Translations', 'Flights']);
+        $booking = $this->Bookings->get($id, contain: ['Users', 'Insurances', 'Hotels', 'CarRentals', 'Translations', 'Flights', 'Payments']);
         $this->set(compact('booking'));
     }
 
@@ -98,13 +94,12 @@ class BookingsController extends AppController
             $this->Flash->error(__('The booking could not be saved. Please, try again.'));
         }
         $users = $this->Bookings->Users->find('list', limit: 200)->all();
-        $payments = $this->Bookings->Payments->find('list', limit: 200)->all();
         $insurances = $this->Bookings->Insurances->find('list', limit: 200)->all();
         $hotels = $this->Bookings->Hotels->find('list', limit: 200)->all();
         $carRentals = $this->Bookings->CarRentals->find('list', limit: 200)->all();
         $translations = $this->Bookings->Translations->find('list', limit: 200)->all();
         $flights = $this->Bookings->Flights->find('list', limit: 200)->all();
-        $this->set(compact('booking', 'users', 'payments', 'insurances', 'hotels', 'carRentals', 'translations', 'flights'));
+        $this->set(compact('booking', 'users', 'insurances', 'hotels', 'carRentals', 'translations', 'flights'));
     }
 
     /**
@@ -116,7 +111,7 @@ class BookingsController extends AppController
      */
     public function edit($id = null)
     {
-        $booking = $this->Bookings->get($id, contain: ['Flights']);
+        $booking = $this->Bookings->get($id, contain: []);
         if ($this->request->is(['patch', 'post', 'put'])) {
             $booking = $this->Bookings->patchEntity($booking, $this->request->getData());
             if ($this->Bookings->save($booking)) {
@@ -127,13 +122,12 @@ class BookingsController extends AppController
             $this->Flash->error(__('The booking could not be saved. Please, try again.'));
         }
         $users = $this->Bookings->Users->find('list', limit: 200)->all();
-        $payments = $this->Bookings->Payments->find('list', limit: 200)->all();
         $insurances = $this->Bookings->Insurances->find('list', limit: 200)->all();
         $hotels = $this->Bookings->Hotels->find('list', limit: 200)->all();
         $carRentals = $this->Bookings->CarRentals->find('list', limit: 200)->all();
         $translations = $this->Bookings->Translations->find('list', limit: 200)->all();
         $flights = $this->Bookings->Flights->find('list', limit: 200)->all();
-        $this->set(compact('booking', 'users', 'payments', 'insurances', 'hotels', 'carRentals', 'translations', 'flights'));
+        $this->set(compact('booking', 'users', 'insurances', 'hotels', 'carRentals', 'translations', 'flights'));
     }
 
     /**
