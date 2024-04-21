f13b354a67696439f3754dedd9bab80745ece909
Antony Mangala <aman0083@student.monash.edu>
2024-04-21 20:36:28 +1000

    Changed id label to reference # to be more useful than pulling from db

--- templates/ContactForms/index.php
+++ templates/ContactForms/index.php
@@ -82,7 +82,7 @@
         <table>
             <thead>
                 <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
+                    <th><?= $this->Paginator->sort('id', 'Reference #') ?></th>
                     <th><?= $this->Paginator->sort('email') ?></th>
                     <th><?= $this->Paginator->sort('phone_number') ?></th>
                     <th><?= $this->Paginator->sort('first_name') ?></th>
