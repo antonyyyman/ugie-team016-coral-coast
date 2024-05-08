27f1523e3cfc7818ff386e137c6e025c7e35b126
jiahe <jmaa0079@student.monash.edu>
2024-05-08 20:52:08 +1000

    fixed error, changed the default value

--- templates/Bookings/add.php
+++ templates/Bookings/add.php
@@ -34,9 +34,9 @@ $this->setLayout("defaultadmin");
                     echo $this->Form->control('translation_id', ['options' => $translations, 'empty' => true]);
                     echo $this->Form->control('payment_id');
                     echo $this->Form->control('travel_deal_id');
-//                    echo $this->Form->control('total_price');
+                    echo $this->Form->control('total_price');
 //                    echo $this->Form->control('booking_status');
-                    echo $this->Form->hidden('booking_status', ['value' => 'active']);
+                    echo $this->Form->hidden('booking_status', ['value' => '1']);
 
                 ?>
             </fieldset>
